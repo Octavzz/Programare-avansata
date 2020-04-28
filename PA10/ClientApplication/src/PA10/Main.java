7745183a2d5774dd20161eedc8125b5bc10ea183
Octavian Iacob <octavian.iacob@icloud.com>
2020-04-28 19:38:46 +0300

    Lab10 Compulsory

---
+++ PA10/ClientApplication/src/PA10/Main.java
@@ -0,0 +1,12 @@
+package PA10;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+	    GameClient g = new GameClient();
+	    g.main("test");
+	    g.main("stop");
+    }
+}
---
+++ PA10/ServerApplication/src/PA10/GameServer.java
@@ -0,0 +1,25 @@
+package PA10;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class GameServer {
+    public static final int PORT = 8100;
+    public GameServer() throws IOException {
+        ServerSocket serverSocket = null ;
+        try {
+            serverSocket = new ServerSocket(PORT);
+            while (true) {
+                System.out.println ("Waiting for a client ...");
+                Socket socket = serverSocket.accept();
+                // Execute the client's request in a new thread
+                new ClientThread(socket).start();
+            }
+        } catch (IOException e) {
+            System.err. println ("Ooops... " + e);
+        } finally {
+            serverSocket.close();
+        }
+    }
+}
